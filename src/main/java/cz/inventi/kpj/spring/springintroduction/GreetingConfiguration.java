9009cf78a525d585a9ce95a3daf87199babe43b5
Vojtech Leskovsky <Leskivojt@gmail.com>
2023-10-08 23:45:02 +0200

    version with moved Main Class

--- src/main/java/cz/inventi/kpj/spring/springintroduction/GreetingConfiguration.java
+++ src/main/java/cz/inventi/kpj/spring/springintroduction/GreetingConfiguration.java
@@ -4,13 +4,12 @@ import cz.inventi.kpj.spring.springintroduction.impl.GreetingConstantRepositoryI
 import cz.inventi.kpj.spring.springintroduction.persistence.GreetingRepository;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
 @EnableConfigurationProperties(GreetingConfigurationProperties.class)
-@ComponentScan(basePackages = "cz.inventi.kpj.spring.springintroduction")
 
 
 
--- src/main/java/cz/inventi/kpj/spring/springintroduction/main/SpringIntroductionApplication.java
+++ src/main/java/cz/inventi/kpj/spring/springintroduction/main/SpringIntroductionApplication.java
@@ -1,10 +1,13 @@
-package cz.inventi.kpj.spring.springintroduction;
+package cz.inventi.kpj.spring.springintroduction.main;
 
 import cz.inventi.kpj.spring.springintroduction.service.GreetingService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "cz.inventi.kpj.spring.springintroduction")
+
 
 public class SpringIntroductionApplication {
 
