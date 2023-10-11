05bbd4dfe3ef3137672c4e67f749eae240a7c6e1
Marek Raja <rajamarek@gmail.com>
2023-10-11 22:54:53 +0200

    marek_raja

--- src/main/java/cz/inventi/kpj/spring/springintroduction/persistence/GreetingConstantRepositoryImpl.java
+++ src/main/java/cz/inventi/kpj/spring/springintroduction/persistence/GreetingConstantRepositoryImpl.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Component;
 @Component("greetingConstantRepository")
 public class GreetingConstantRepositoryImpl implements GreetingRepository {
 
+    //abs
+
     @Override
     public String getGreetingText() {
         return "Hello from Mongo";
